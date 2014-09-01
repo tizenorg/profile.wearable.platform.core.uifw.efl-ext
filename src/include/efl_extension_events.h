91847aae12c437b72eb0954ed4b364064f229513
ChunEon Park <chuneon.park@samsung.com>
2014-09-01 17:50:07 +0900

    events: update documentation.
    Change-Id: I99cb36a5e03e92c5a6445e880ed1b1b0e2132af7

--- src/include/efl_extension_events.h
+++ src/include/efl_extension_events.h
@@ -28,6 +28,7 @@ extern "C" {
  * @defgroup EFL_EXTENSION_EVENTS_GROUP Efl Extension Event
  * @ingroup EFL_EXTENSION_GROUP
  * @brief This group provides functionalities to handle back/send key events.
+ * @since_tizen 2.3
  *
  * @{
  */
@@ -36,7 +37,11 @@ extern "C" {
  * @brief Convenient macro function that sends back key events to the popup
  *        to be removed.
  *
+ * @details Popup will be removed when it has the back key event with
+ *          eext_object_event_callback_add()
+ *
  * @see   eext_object_event_callback_add()
+ * @since_tizen 2.3
  */
 static inline void
 eext_popup_back_cb(void *data, Evas_Object *obj, void *event_info)
@@ -48,7 +53,11 @@ eext_popup_back_cb(void *data, Evas_Object *obj, void *event_info)
  * @brief Convenient macro function that sends back key events to the ctxpopup
  *        to be dismissed.
  *
+ * @details Ctxpopup will be dismissed when it has the back key event with
+ *          eext_object_event_callback_add()
+ *
  * @see   eext_object_event_callback_add()
+ * @since_tizen 2.3
  */
 static inline void
 eext_ctxpopup_back_cb(void *data, Evas_Object *obj, void *event_info)
@@ -59,7 +68,12 @@ eext_ctxpopup_back_cb(void *data, Evas_Object *obj, void *event_info)
 /**
  * @brief Convenient macro function that sends more key events to the naviframe
  *        top item.
+ *
+ * @details Ctxpopup will be shown/hidden when naviframe has the more key event
+ *          with eext_object_event_callback_add()
+ *
  * @see   eext_object_event_callback_add()
+ * @since_tizen 2.3
  */
 static inline void
 eext_naviframe_more_cb(void *data, Evas_Object *obj, void *event_info)
@@ -76,7 +90,12 @@ eext_naviframe_more_cb(void *data, Evas_Object *obj, void *event_info)
 
 /**
  * @brief Convenient macro function that pop the naviframe item.
+ *
+ * @details Naviframe will be popped when naviframe has the back key event
+ *          with eext_object_event_callback_add()
+ *
  * @see   eext_object_event_callback_add()
+ * @since_tizen 2.3
  */
 static inline void
 eext_naviframe_back_cb(void *data, Evas_Object *obj, void *event_info)
@@ -89,19 +108,36 @@ eext_naviframe_back_cb(void *data, Evas_Object *obj, void *event_info)
  *
  * @see eext_object_event_callback_add()
  * @see eext_object_event_callback_del()
+ * @since_tizen 2.3
  */
 typedef enum _Eext_Callback_Type
 {
-   EEXT_CALLBACK_BACK,
-   EEXT_CALLBACK_MORE,
-   EEXT_CALLBACK_LAST
+   EEXT_CALLBACK_BACK,    /***< H/W Back Key Event */
+   EEXT_CALLBACK_MORE,    /***< H/W More Key Event */
+   EEXT_CALLBACK_LAST     /***< Reserved value. Acutally This is not matched to
+                            any meaningful behavior. */
 } Eext_Callback_Type;
 
-/** <Ea event callback function signature */
+/**
+ * @brief Ea event callback function signature.
+ * @param[in] data user data
+ * @param[in] obj target object
+ * @param[in] event_info event information (if the event passes an additional in
+              formation.)
+ * @see eext_object_event_callback_add()
+ * @since_tizen 2.3
+ */
 typedef void (*Eext_Event_Cb)(void *data, Evas_Object *obj, void *event_info);
 
 /**
- * Delete a callback function from an object.
+ * @brief Delete a callback function from an object.
+ *
+ * @details This function removes the most recently added callback from the
+ *          object @p obj which was triggered by the type @p type and
+ *          was calling the function @p func when triggered. If the removal is
+ *          successful it will also return the data pointer that was passed to
+ *          eext_object_event_callback_add() when the callback was added to
+ *          the object. If not successful @c NULl will be returned.
  *
  * @param[in] obj Object to remove a callback from.
  * @param[in] type The type of event that was triggering the callback.
@@ -109,50 +145,44 @@ typedef void (*Eext_Event_Cb)(void *data, Evas_Object *obj, void *event_info);
  *            triggered
  * @return    data The data pointer that was to be passed to the callback.
  *
- * @brief     This function removes the most recently added callback from the
- *            object @p obj which was triggered by the type @p type and
- *            was calling the function @p func when triggered. If the removal is
- *            successful it will also return the data pointer that was passed to
- *            eext_object_event_callback_add() when the callback was added to
- *            the object. If not successful @c NULl will be returned.
- *
  * @see eext_object_event_callback_add()
+ * @since_tizen 2.3
  */
-
 EAPI void *eext_object_event_callback_del(Evas_Object *obj, Eext_Callback_Type type, Eext_Event_Cb);
 
 /**
- * Add (register) a callback function to a given evas object.
+ * @brief Add (register) a callback function to a given evas object.
+ *
+ * @details This function adds a function callback to an object when the key
+ *          event occurs on object @p obj. The key event on the object is only
+ *          triggered when the object is the most top in objects stack and
+ *          visible. This means, like the naviframe widget, if your application
+ *          needs to have the events based on the view but not focus, you can
+ *          use this callback. A callback function must have the Eext_Event_Cb
+ *          prototype definition. The first parameter (@p data) in this
+ *          definition will have the same value passed to
+ *          eext_object_event_callback_add() as the @p data parameter, at
+ *          runtime. The second parameter @p obj is the evas object on which
+ *          event occurred. Finally, the third parameter @p event_info is a
+ *          pointer to a data structure that may or may not be passed to the
+ *          callback, depending on the event type that triggered the callback.
+ *          This is so because some events don't carry extra context with them,
+ *          but others do.
  *
  * @param[in] obj evas object.
  * @param[in] type The type of event that will trigger the callback.
  * @param[in] func The function to be called when the key event is triggered.
  * @param[in] data The data pointer to be passed to @p func.
  *
- * @brief This function adds a function callback to an object when the key event
- *        occurs on object @p obj. The key event on the object is only triggered
- *        when the object is the most top in objects stack and visible. This
- *        means, like the naviframe widget, if your application needs to have
- *        the events based on the view but not focus, you can use this callback.
- *
- *        A callback function must have the Eext_Event_Cb prototype definition.
- *        The first parameter (@p data) in this definition will have the same
- *        value passed to eext_object_event_callback_add() as the @p data
- *        parameter, at runtime. The second parameter @p obj is the evas object
- *        on which event occurred. Finally, the third parameter @p event_info is
- *        a pointer to a data structure that may or may not be passed to the
- *        callback, depending on the event type that triggered the callback.
- *        This is so because some events don't carry extra context with them,
- *        but others do.
- *
  * @see eext_object_event_callback_del()
+ * @since_tizen 2.3
  */
-
 EAPI void eext_object_event_callback_add(Evas_Object *obj, Eext_Callback_Type type, Eext_Event_Cb func, void *data);
 
 /**
  * @brief Convenient macro function that cancel the selection of the entry.
  * @see   eext_entry_selection_back_event_allow_set()
+ * @since_tizen 2.3
  */
 static inline void
 eext_entry_back_cb(void *data, Evas_Object *obj, void *event_info)
@@ -167,6 +197,7 @@ eext_entry_back_cb(void *data, Evas_Object *obj, void *event_info)
 /**
  * @brief Convenient macro function that cancel the selection of the entry.
  * @see   eext_entry_selection_back_event_allow_set()
+ * @since_tizen 2.3
  */
 static inline void
 eext_entry_selection_start_cb(void *data, Evas_Object *obj, void *event_info)
@@ -181,7 +212,13 @@ eext_entry_selection_start_cb(void *data, Evas_Object *obj, void *event_info)
 
 /**
  * @brief Convenient macro function that cancel the selection of the entry.
+ *
+ * @details If the selection handler should be canceled when back key event is
+ *          triggered, then use this API.
+ *
  * @see   eext_entry_selection_back_event_allow_set()
+ * @see   eext_object_event_callback_add()
+ * @since_tizen 2.3
  */
 static inline void
 eext_entry_selection_cleared_cb(void *data, Evas_Object *obj, void *event_info)
@@ -196,11 +233,14 @@ eext_entry_selection_cleared_cb(void *data, Evas_Object *obj, void *event_info)
 /**
  * @brief Convenient macro function that handle the back event to cancel the
  *        selection handler of the entry.
- * @param obj   Entry object.
- * @param allow @c EINA_TRUE allows the back event handling, @c EINA_FALSE
+ * @details If the selection handler should be canceled (or not) when back key
+ *          event is triggered, then use this API.
+ * @param[in] obj Entry object.
+ * @param[in] allow @c EINA_TRUE allows the back event handling, @c EINA_FALSE
  *              otherwise.
  * @see   eext_object_event_callback_add()
  * @see   eext_object_event_callback_del()
+ * @since_tizen 2.3
  */
 static inline void
 eext_entry_selection_back_event_allow_set(Evas_Object *obj, Eina_Bool allow)
